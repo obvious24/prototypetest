a1c47a223df3b3cc3c32e0392f832ea6b30a2098
WakatsukiYuto <wakatsuki.yuto.kk@comsis.co.jp>
2021-01-25 13:05:15 +0900

    bootstrap入れた

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 createApp(App).mount('#app')
